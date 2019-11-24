632bf086461e102b69a71ab78c7d136abe89da25
muhafaa <muhafa99@gmail.com>
2019-11-24 09:31:27 +0700

    update sound and alert box

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 var target = document.getElementById("target");
 var startBtn = document.getElementById("startBtn");
 var x;
+var btnSound = document.getElementById("button-sound");
 
+// button start on click
 function start() {
-  swal("Click the circle target as many as you can!").then(value => {
+  btnSound.play();
+  swal({
+    title: "Instruction!",
+    text: "Click on the circle target before the timer is up"
+  }).then(value => {
+    btnSound.play();
     let time = 9;
     randomized();
     target.style.visibility = "unset";
@@ -16,19 +23,25 @@ function start() {
 var click = 0;
 var c = document.getElementById("click");
 
+// score
 function move() {
+  let audio = document.getElementById("target-sound");
   randomized();
   click += 1;
   c.innerHTML = "Score : " + click;
+  audio.play();
 }
 
+// button reset on click
 function resetOnClick() {
+  btnSound.play();
   swal({
     title: "Are you sure want to reset?",
     text: "You are about to restart the game",
     buttons: true,
     dangerMode: true
   }).then(value => {
+    btnSound.play();
     if (value) {
       reset();
     }
@@ -48,6 +61,7 @@ function reset() {
 var targetWidth = target.style.width;
 var targetHeight = target.style.height;
 
+// target randomized effect
 function randomized() {
   let heightMax = 55;
   let widthMax = 74;
@@ -64,6 +78,7 @@ function randomized() {
   target.style.backgroundColor = color;
 }
 
+// timer
 function timer(seconds) {
   x = setInterval(function() {
     let timer = document.getElementById("timer");
@@ -71,6 +86,13 @@ function timer(seconds) {
     timer.innerHTML = "Timer : " + seconds + " second(s)";
 
     if (seconds == 0) {
+      let audio = document.getElementById("timer-sound");
+      audio.play();
+      swal({
+        title: "Time's Up!",
+        text: "Your Score : " + click,
+        icon: "warning"
+      });
       if (click > highScore.innerHTML) {
         highScore.innerHTML = click;
       }
